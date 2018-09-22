dd193e8f07bbf33962e9cf8a67566655fd2b05ea
Themba Dube <embeddedthemba@gmail.com>
2018-09-22 16:03:37 -0400

    Add Lua test script

--- core/Main.cpp
+++ core/Main.cpp
@@ -5,12 +5,51 @@
 
 #include <GL/gl.h>
 
+#include <lua5.2/lua.hpp>
+
+static void print_error(lua_State* state) {
+  // The error message is on top of the stack.
+  // Fetch it, print it and then pop it off the stack.
+  const char* message = lua_tostring(state, -1);
+  puts(message);
+  lua_pop(state, 1);
+}
+
+static void execute(const char* filename)
+{
+  lua_State *state = luaL_newstate();
+
+  // Make standard libraries available in the Lua object
+  luaL_openlibs(state);
+
+  int result;
+
+  // Load the program; this supports both source code and bytecode files.
+  result = luaL_loadfile(state, filename);
+
+  if ( result != LUA_OK ) {
+    print_error(state);
+    return;
+  }
+
+  // Finally, execute the program by calling into it.
+  // Change the arguments if you're not running vanilla Lua code.
+
+  result = lua_pcall(state, 0, LUA_MULTRET, 0);
+
+  if ( result != LUA_OK ) {
+    print_error(state);
+    return;
+  }
+}
+
 class TestApp : public Application
 {
 public:
     void Create()
     {
         Application::Create();
+	execute("test.lua");
     }
 
     void Draw()
