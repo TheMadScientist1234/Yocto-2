414e54da28fbc170d8c5884b6750724dfc81232b
TheMadScientist123 <joshua.t.woodie@gmail.com>
2018-09-23 11:12:59 -0400

    Create moving test

--- core/Main.cpp
+++ core/Main.cpp
@@ -27,6 +27,10 @@ public:
         glLoadIdentity();
 
         texture = new Texture("test.png");
+        x = 0;
+        y = 0;
+        xdir = 0;
+        ydir = 0;
     }
 
     void Draw()
@@ -34,7 +38,25 @@ public:
         glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        texture->draw(0, 0);
+        if(xdir == 0)
+            x += 2;
+        if(xdir == 1)
+            x -= 2;
+        if(ydir == 0)
+            y += 2;
+        if(ydir == 1)
+            y -= 2;
+        
+        if(x + texture->getWidth() == width)
+            xdir = 1;
+        if(x < 0)
+            xdir = 0;
+        if(y + texture->getHeight() == height)
+            ydir = 1;
+        if(y < 0)
+            ydir = 0;
+
+        texture->draw(x, y);
 
         Application::Draw();
     }
@@ -45,6 +67,8 @@ public:
     }
 private:
     Texture* texture = nullptr;
+    int x, y;
+    int xdir, ydir;
 };
 
 int main()
