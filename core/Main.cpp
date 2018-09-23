148650feb780111812115fa92f59abe5b43b776c
TheMadScientist123 <joshua.t.woodie@gmail.com>
2018-09-23 10:23:45 -0400

    Graphics  test code

--- core/Main.cpp
+++ core/Main.cpp
@@ -2,8 +2,11 @@
 #include <iostream>
 
 #include "Application.hpp"
+#include "../file/BitmapLoader.hpp"
 
 #include <GL/gl.h>
+
+#include <libpng16/png.h>
 
 class TestApp : public Application
 {
@@ -11,12 +14,50 @@ public:
     void Create()
     {
         Application::Create();
+
+        glViewport(0, 0, width, height);
+
+        glEnable(GL_TEXTURE_2D);
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+        glOrtho(0, width, height, 0, 0.0f, 1.0f);
+
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+
+        glBindTexture(GL_TEXTURE_2D, 0);
     }
 
     void Draw()
     {
         glClearColor(0, 0, 0, 1);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBegin(GL_QUADS);
+            glTexCoord2d(0, 0);
+            glVertex2i(0, 0);
+
+            glTexCoord2d(0, 1);
+            glVertex2i(0, 64);
+
+            glTexCoord2d(1, 1);
+            glVertex2i(64, 64);
+
+            glTexCoord2d(1, 0);
+            glVertex2i(64, 0);
+        glEnd();
+        glBindTexture(GL_TEXTURE_2D, 0);
 
         Application::Draw();
     }
@@ -25,6 +66,8 @@ public:
     {
         Application::Dispose();
     }
+private:
+    GLuint texture = 0;
 };
 
 int main()
